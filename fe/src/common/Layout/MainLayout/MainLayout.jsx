7fc7f3fcc7588d415744ab91fad95169473f0c0b
Anhpika <anhpika12345@gmail.com>
2026-06-03 10:26:17 +0700

    Enhance MainLayout to conditionally render HomeSidebar based on FriendsPage route; add new workspace-single style for improved layout.

--- fe/src/common/Layout/MainLayout/MainLayout.jsx
+++ fe/src/common/Layout/MainLayout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useMatch } from "react-router-dom";
 import MainHeader from "@/common/Header/MainHeader/MainHeader";
 import MainSidebar from "@/common/Sidebar/MainSidebar/MainSidebar";
 import HomeSidebar from "@/common/Sidebar/HomeSidebar/HomeSidebar";
@@ -6,11 +6,13 @@ import Footer from "@/common/Footer/Footer";
 import styles from "./MainLayout.module.css";
 
 function MainLayout() {
+  const isFriendsPage = useMatch("/home/friends");
+
   return (
     <div className={styles.layout}>
       <MainHeader />
 
-      <div className={styles.workspace}>
+      <div className={`${styles.workspace} ${isFriendsPage ? styles["workspace-single"] : ""}`}>
         <div className={styles["col-left"]}>
           <MainSidebar />
         </div>
@@ -19,9 +21,11 @@ function MainLayout() {
           <Outlet />
         </main>
 
-        <div className={styles["col-right"]}>
-          <HomeSidebar />
-        </div>
+        {!isFriendsPage && (
+          <div className={styles["col-right"]}>
+            <HomeSidebar />
+          </div>
+        )}
       </div>
 
       <Footer />
