2440aaf81f2f9eaf9b1713f3c386800e68a436f8
Anhpika <anhpika12345@gmail.com>
2026-06-03 10:37:32 +0700

    Refactor MainLayout to conditionally render sidebars based on profile and friends pages; add profile mock data and activity heatmap component for enhanced user profile experience.

--- fe/src/common/Layout/MainLayout/MainLayout.jsx
+++ fe/src/common/Layout/MainLayout/MainLayout.jsx
@@ -7,28 +7,34 @@ import styles from "./MainLayout.module.css";
 
 function MainLayout() {
   const isFriendsPage = useMatch("/home/friends");
+  const isProfilePage = useMatch("/profile/:username");
+  const hideSidebars = isFriendsPage || isProfilePage;
 
   return (
     <div className={styles.layout}>
       <MainHeader />
 
-      <div className={`${styles.workspace} ${isFriendsPage ? styles["workspace-single"] : ""}`}>
-        <div className={styles["col-left"]}>
-          <MainSidebar />
-        </div>
+      <div
+        className={`${styles.workspace} ${hideSidebars ? styles["workspace-single"] : ""} ${isProfilePage ? styles["workspace-profile"] : ""}`}
+      >
+        {!isProfilePage && (
+          <div className={styles["col-left"]}>
+            <MainSidebar />
+          </div>
+        )}
 
         <main className={styles["col-center"]} id="home-top">
           <Outlet />
         </main>
 
-        {!isFriendsPage && (
+        {!hideSidebars && (
           <div className={styles["col-right"]}>
             <HomeSidebar />
           </div>
         )}
       </div>
 
-      <Footer />
+      {!isProfilePage && <Footer />}
     </div>
   );
 }
