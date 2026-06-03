14f8c6165d8cc94790078e7ec5489fc935c08a90
Ngo Phuoc Toan <tngo28299@gmail.com>
2026-06-03 09:40:29 +0700

    Add Button component and Button css

---
+++ fe/src/common/Button/Button.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import styles from "./Button.module.css";
+
+const LOOK_CLASS = {
+  solid: styles.solid,
+  outline: styles.outline,
+  soft: styles.soft,
+};
+
+const SIZE_CLASS = {
+  sm: styles.sm,
+  md: "",
+  lg: styles.lg,
+};
+
+function Button({
+  children,
+  look = "solid",
+  size = "md",
+  fullWidth = false,
+  to,
+  type = "button",
+  disabled = false,
+  onClick,
+  className = "",
+}) {
+  const classes = [
+    styles.btn,
+    LOOK_CLASS[look],
+    SIZE_CLASS[size],
+    fullWidth && styles.full,
+    className,
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  if (to) {
+    return (
+      <Link className={classes} to={to}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button type={type} className={classes} disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
