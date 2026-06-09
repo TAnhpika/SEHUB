e4e7bde304515f2712b1706a1f9bc3a21e696439
Ngo Phuoc Toan <tngo28299@gmail.com>
2026-06-09 16:58:23 +0700

    Fix Route Guest

--- fe/src/common/guards/PrivateRoute.jsx
+++ fe/src/common/guards/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { hasStoredSession } from "@/context/AuthProvider";
 import { useAuth } from "@/context";
+import { mapHomeSubjectPathToCommunity } from "@/utils/subjectPaths";
 
 function PrivateRoute() {
   const { isAuthenticated } = useAuth();
@@ -8,6 +9,16 @@ function PrivateRoute() {
   const authed = isAuthenticated || hasStoredSession();
 
   if (!authed) {
+    const communityPath = mapHomeSubjectPathToCommunity(location.pathname);
+    if (communityPath) {
+      return (
+        <Navigate
+          to={`${communityPath}${location.search}`}
+          replace
+        />
+      );
+    }
+
     return <Navigate to="/login" state={{ from: location.pathname }} replace />;
   }
 
