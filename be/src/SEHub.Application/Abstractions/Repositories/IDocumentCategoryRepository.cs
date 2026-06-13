ff6bf420e363bef0f8e5d1b9375f5f69abc4639b
mactuhau <tuhaumac2003@gmail.com>
2026-06-13 17:41:28 +0700

    chore: batch 5/56 - IDocumentAccessLogRepository.cs, IDocumentCategoryRepository.cs, IExamAttemptRepository.cs

--- be/src/SEHub.Application/Abstractions/Repositories/IDocumentCategoryRepository.cs
+++ be/src/SEHub.Application/Abstractions/Repositories/IDocumentCategoryRepository.cs
@@ -5,5 +5,7 @@ namespace SEHub.Application.Abstractions.Repositories;
 public interface IDocumentCategoryRepository
 {
     Task<DocumentCategory?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<DocumentCategory?> FindBySubjectCodeAsync(string subjectCode, CancellationToken cancellationToken = default);
     Task<IReadOnlyList<DocumentCategory>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task AddAsync(DocumentCategory category, CancellationToken cancellationToken = default);
 }
