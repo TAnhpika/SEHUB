19b6ed01294cda03c3c85ad7a9bdc619c045db87
mactuhau <tuhaumac2003@gmail.com>
2026-06-13 17:41:28 +0700

    chore: batch 4/56 - IProfileSnapshotCache.cs, IBadgeRepository.cs, ICommentRepository.cs

--- be/src/SEHub.Application/Abstractions/Repositories/IBadgeRepository.cs
+++ be/src/SEHub.Application/Abstractions/Repositories/IBadgeRepository.cs
@@ -5,4 +5,5 @@ namespace SEHub.Application.Abstractions.Repositories;
 public interface IBadgeRepository
 {
     Task<IReadOnlyList<Badge>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<Badge?> GetByCodeAsync(string code, CancellationToken cancellationToken = default);
 }
