cbd43f2c32fc79e960b25bf612356ecd683a2c09
hikaritakt <126213808+hikaritakt@users.noreply.github.com>
2026-06-19 20:04:52 +0700

    feat(be): configure Question multi-select columns in EF

--- be/src/SEHub.Infrastructure/Persistence/Configurations/QuestionConfiguration.cs
+++ be/src/SEHub.Infrastructure/Persistence/Configurations/QuestionConfiguration.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SEHub.Domain.Entities;
+using SEHub.Domain.Enums;
 
 namespace SEHub.Infrastructure.Persistence.Configurations;
 
@@ -11,6 +12,8 @@ public class QuestionConfiguration : IEntityTypeConfiguration<Question>
         builder.HasKey(q => q.Id);
         builder.HasIndex(q => new { q.ExamId, q.OrderIndex });
         builder.Property(q => q.Content).HasMaxLength(4000).IsRequired();
+        builder.Property(q => q.QuestionType).HasConversion<int>().HasDefaultValue(QuestionType.SingleChoice);
+        builder.Property(q => q.CorrectOptionIdsJson).HasMaxLength(2000).HasDefaultValue("[]");
 
         builder.HasMany(q => q.Options)
             .WithOne(o => o.Question)
